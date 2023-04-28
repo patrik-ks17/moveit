10e75d369a13603a972b5d6656177457a7e8902e
KasaPatrik <kasaip02@gmail.com>
2023-04-28 08:33:03 +0200

    profiledit test

--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -1,6 +1,4 @@
-
-
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom'
 import Profile from "../components/profile/Profile";
 
